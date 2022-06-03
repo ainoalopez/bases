254c6cc630f19a96905cc1f911af2a54f4303c3b
ainoalopez <ainoalopez@iti.es>
2022-06-03 09:56:05 +0200

    cambios en el template del componente heroe

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -9,5 +9,15 @@ import { Component } from "@angular/core";
 
 export class HeroeComponent {
 
+    nombre: string = 'Ironman';
+    edad  : number = 45;
+
+    get nombreCapitalizado() {
+        return this.nombre.toUpperCase();
+    }
+
+    obtenerNombre(): string {
+        return `${ this.nombre } - ${ this.edad }`
+    }
 
 }
