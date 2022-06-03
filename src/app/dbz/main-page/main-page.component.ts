aad577f6d3f5f9ee68aff4308ea425982b0a6c85
ainoalopez <ainoalopez@iti.es>
2022-06-03 13:19:17 +0200

    ngModel (two way data binding)

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
 
+  nuevo: Personaje = {
+    nombre: 'Trunks',
+    poder: 14000
+  }
+
+  cambiarNombre( event: any){
+    console.log( event.target.value );
+    
+  }
+
   agregar() {
-    console.log('esta es una prueba!');
+    console.log(this.nuevo);
   }
 
 }
