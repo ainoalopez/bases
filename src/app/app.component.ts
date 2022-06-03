b636f9603219f4441ac534a50b044fc88a811c4d
ainoalopez <ainoalopez@iti.es>
2022-06-03 09:32:45 +0200

    Creo manualmente un componente contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,5 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent {
-  titulo: string = 'Contador App';
-  numero: number = 10;
-  base  : number = 5;
 
-  acumular( valor: number ) {
-    this.numero += valor;
-  }
-}
+export class AppComponent {}
